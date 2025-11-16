e1e2a94584cf55531a378886c04ef4b77618b7c0
pedrofranca0509 <85133892+pedrofranca0509@users.noreply.github.com>
2025-11-16 00:34:34 -0300

    Creating the homepage and login screen

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,16 +5,42 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.graphics.Color
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val texto = findViewById<TextView>(R.id.titulo)
+        val palavra = "FireFit"
+        val span = SpannableString(palavra)
+
+        // "Fire" = #FF4601
+        span.setSpan(
+            ForegroundColorSpan(Color.parseColor("#FF4601")),
+            0, 4,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        // "Fit" = #FFFFFF
+        span.setSpan(
+            ForegroundColorSpan(Color.parseColor("#FFFFFF")),
+            4, palavra.length,
+            Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+
+        texto.text = span
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
     }
 }
