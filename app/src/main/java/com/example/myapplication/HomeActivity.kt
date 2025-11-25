15216de7da6c61d436e396fd93f6be68202e59f2
pedrofranca0509 <85133892+pedrofranca0509@users.noreply.github.com>
2025-11-24 22:37:08 -0300

    navigation bar working

--- app/src/main/java/com/example/myapplication/HomeActivity.kt
+++ app/src/main/java/com/example/myapplication/HomeActivity.kt
@@ -1,13 +1,34 @@
 package com.example.myapplication
 
-
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+
+        // Deixar HOME selecionado
+        bottomNav.selectedItemId = R.id.nav_home
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.nav_home -> true   // já estamos aqui
+
+                R.id.nav_user -> {
+                    startActivity(Intent(this, UserActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+
+                else -> false
+            }
+        }
     }
 }
--- app/src/main/java/com/example/myapplication/UserActivity.kt
+++ app/src/main/java/com/example/myapplication/UserActivity.kt
@@ -1,15 +1,35 @@
-package com.seuprojeto.firefit
+package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.myapplication.R
 
 class UserActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user)
 
+        val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNav)
+
+        // Deixar o item de usuário selecionado
+        bottomNav.selectedItemId = R.id.nav_user
+
+        bottomNav.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+
+                R.id.nav_home -> {
+                    startActivity(Intent(this, HomeActivity::class.java))
+                    overridePendingTransition(0, 0)
+                    true
+                }
+
+                R.id.nav_user -> true   // já estamos nessa tela
+
+                else -> false
+            }
         }
     }
-
+}
